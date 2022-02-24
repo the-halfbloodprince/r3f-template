e642002482b2e4db62a79e3ad54512162df9eca3
Manish Kumar Das <ammanishkumardas@gmail.com>
2022-02-24 23:54:36 +0530

    intro component

---
+++ components/Introduction.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '../styles/Introduction.module.scss'
+
+const Introduction = () => {
+  return (
+    <div className={styles.container} >
+        <p>This is an octahedron</p>
+        <p>Don't count. It really has 8 sides.</p>
+    </div>
+  )
+}
+
+export default Introduction
